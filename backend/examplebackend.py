a8b29f93ad69d69ad0a5b83d5b63705f8c03c699
FluidSense <johanneskv96@gmail.com>
2019-01-23 14:21:48 +0100

    create example of flask and sqlalchemy

--- backend/examplebackend.py
+++ backend/examplebackend.py
@@ -1,18 +1,50 @@
-from flask import Flask, request
-from flask_sqlalchemy import SQLAlchemy
-from models import showcase 
+import json
+from flask import Flask, Response, request, abort
+from shared import db
+from models.showcase import Showcase
 
-app=Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://bachelor:07idistudadm@localhost:5432/bachelor'
+database_file = "postgresql://bachelor_usr:07idistudadm@localhost:5432/bachelor"
 
+app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Suk my dik"
+app.config["SQLALCHEMY_DATABASE_URI"] = database_file
 
-@app.route("/getExample", methods = ['GET'])
-def new():
-    if request.method == 'GET':
-        result = showcase.query.all()
-        return result
+db.init_app(app)
 
+@app.route("/",methods=["GET"])
+def display():
+    result = Showcase.query.all()
+    return ", ".join(str(e.id) for e in result)
+
+@app.route("/post",methods=["POST"])
+def reveerb():
+    if request.form:
+        thing = Showcase(name=request.form.get("name"))
+        db.session.add(thing)
+        db.session.commit()
+        return Response(request.form, status=201)
+
+@app.route("/update", methods=["PUT"])
+def skateboards():
+    if request.method == "PUT" and request.data:
+        data = json.loads(request.data)
+        if not data.get("id") or not data.get("name"):
+            return abort(400)
+        dbthing = db.session.query(Showcase).get(data["id"])
+        dbthing.name = data["name"]
+        db.session.add(dbthing)
+        db.session.commit()
+        return Response("OK", status=202)
+
+@app.route("/delete", methods=["DELETE"])
+def tea():
+    if request.data:
+        data = json.loads(request.data)
+        if not data.get("name"):
+            return abort(400)
+        dbthing = db.session.query(Showcase).filter_by(name=data["name"]).first()
+        if not dbthing:
+            return abort(404)
+        db.session.delete(dbthing)
+        db.session.commit()
+        return Response("DELETED", status=202)
--- backend/models/showcase.py
+++ backend/models/showcase.py
@@ -1,12 +1,10 @@
-from sqlalchemy import Column, Integer, String
+from shared import db
 
-from database import Base
-
-class Showcase(Base):
+class Showcase(db.Model):
     __tablename__ = 'showcase_table'
 
-    id = Column('student_id', Integer, primary_key = True)
-    name = Column(String(100))
+    id = db.Column('student_id', db.Integer, primary_key = True)
+    name = db.Column(db.String(100))
 
     def __init__(self, name):
         self.name = name
