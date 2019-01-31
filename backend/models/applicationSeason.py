73c3f27ffff8086fcab1561642b7554f3d9e969a
FluidSense <johanneskv96@gmail.com>
2019-01-31 12:44:50 +0100

    rename examplebackend => app

--- backend/models/applicationSeason.py
+++ backend/models/applicationSeason.py
@@ -6,17 +6,22 @@ class ApplicationSeason(db.Model):
     id = db.Column("application_season_id", db.Integer, primary_key=True)
     start = db.Column("starttime", db.DateTime, default=datetime.today())
     end = db.Column("endtime", db.DateTime, nullable=False)
-    acceptStart = db.Column("acceptstart", db.DateTime, default=datetime.today())
-    acceptEnd = db.Column("acceptend", db.DateTime, nullable=False)
+    applicationPeriodStart = db.Column("applicationperiodstart", db.DateTime, default=datetime.today())
+    applicationPeriodEnd = db.Column("applicationperiodend", db.DateTime, nullable=False)
 
     def __init__(self, start, end, acceptstart, acceptend):
         self.start = start
         self.end = end
-        self.acceptStart = acceptstart
-        self.acceptEnd = acceptend
+        self.applicationPeriodStart = acceptstart
+        self.applicationPeriodEnd = acceptend
 
     def to_json(self):
-        return {"start":self.start,"end":self.end,"acceptStart":self.acceptStart,"acceptEnd":self.acceptEnd}
+        return {
+            "start":self.start,
+            "end":self.end,
+            "applicationPeriodStart":self.applicationPeriodStart,
+            "applicationPeriodEnd":self.applicationPeriodEnd,
+        }
     def __str__(self):
         return str(self.__class__) + ":" + str(self.__dict__)
 
--- backend/services/applicationSeasonService.py
+++ backend/services/applicationSeasonService.py
@@ -1,14 +1,14 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 from shared import db
 from models.applicationSeason import ApplicationSeason
 
 def getCurrentOrNext():
     comingSeasons = db.session.query(ApplicationSeason).filter(ApplicationSeason.end > datetime.today()).all()
-    lowestDiff = timedelta.max
-    currentClosest = None
-    for season in comingSeasons:
-        diff = season.acceptEnd - datetime.today()
-        if diff < lowestDiff:
-            lowestDiff = diff
-            currentClosest = season
-    return currentClosest
+   # lowestDiff = timedelta.max
+   # currentClosest = None
+   # for season in comingSeasons:
+   #     diff = season.acceptEnd - datetime.today()
+   #     if diff < lowestDiff:
+   #         lowestDiff = diff
+   #         currentClosest = season
+    return min(comingSeasons, key=lambda season: season.applicationPeriodStart - datetime.today)
