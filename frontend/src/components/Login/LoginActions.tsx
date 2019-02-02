6bbdcd9ef41ea5aa8e6fe04dcbbd1c968f278bfd
Paalar <paaledward@gmail.com>
2019-02-02 11:41:48 +0100

    Adds FIXME comment for future reference

--- frontend/src/components/Login/LoginActions.tsx
+++ frontend/src/components/Login/LoginActions.tsx
@@ -1,6 +1,7 @@
 import { Dispatch } from 'redux';
 import { SET_USER_DATA } from './Strings';
 
+// FIXME fetchUserData shouldn't be type any
 export const fetchUserData = (userID: number): any => (dispatch: Dispatch) => {
   fetch(`http://localhost:5000/application/user/${userID}`)
     .then(response => response.json())
