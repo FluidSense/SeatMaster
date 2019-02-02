3fafda71409a294d54eb10ae03bc5ba0e7e3942e
Paalar <paaledward@gmail.com>
2019-02-02 14:53:55 +0100

    Reworks return statement

--- frontend/src/components/Login/LoginReducer.tsx
+++ frontend/src/components/Login/LoginReducer.tsx
@@ -24,7 +24,13 @@ export const loginReducer = (
   const { type, payload } = action;
   switch (type) {
     case SET_USER_DATA: {
-      return payload;
+      return {
+        ...state,
+        comments: payload.comments,
+        id: payload.id,
+        status: payload.status,
+        user: payload.user,
+      };
     }
     default:
       return state;
