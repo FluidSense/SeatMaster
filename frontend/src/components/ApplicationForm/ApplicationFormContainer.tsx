a1f14b2e4779038990b4f77630ff98ef2736514b
Peter Rydberg <hmp.rydberg@gmail.com>
2019-02-03 19:02:36 +0100

    #50 added container

---
+++ frontend/src/components/ApplicationForm/ApplicationFormContainer.tsx
@@ -0,0 +1,17 @@
+import { connect } from 'react-redux';
+import ApplicationFormComponent from './ApplicationFormComponent';
+
+const mapStateToProps = (state: any) => ({
+  email: state.userInformation.email,
+  fullname: state.userInformation.fullname,
+  phone: state.userInformation.phone,
+  status: state.userInformation.status,
+  username: state.userInformation.username,
+});
+
+const ApplicationFormContainer = connect(
+  mapStateToProps,
+  null,
+)(ApplicationFormComponent);
+
+export default ApplicationFormContainer;
