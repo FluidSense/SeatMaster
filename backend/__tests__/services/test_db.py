0f27682427cc5b30a18293d81858de78b5e19bd6
FluidSense <johanneskv96@gmail.com>
2019-02-07 12:04:54 +0100

    pass simple username test with db

--- backend/__tests__/services/test_db.py
+++ backend/__tests__/services/test_db.py
@@ -5,18 +5,13 @@ from ...models import User
 from ...main import create_app
 from ...shared import db
 
-def cleanUp():
-    shutil.rmtree("./migrations")
 
-cleanUp()
-
-with testing.postgresql.Postgresql() as new_psql:
-    app = create_app(db_url=new_psql.url())
-    with app.app_context() as app:
-        init()
-        migrate()
-        upgrade()
-        new_user = User("Boost")
-        db.session.add(new_user)
-        db.session.commit()
-        assert new_user.username == db.session.query(User).one().username
+def test_user_submittion():
+    with testing.postgresql.Postgresql() as new_psql:
+        app = create_app(db_url=new_psql.url())
+        with app.app_context() as app:
+            db.create_all()
+            new_user = User("Boost")
+            db.session.add(new_user)
+            db.session.commit()
+            assert new_user.username == db.session.query(User).one().username
