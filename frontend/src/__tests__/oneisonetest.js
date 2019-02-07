78512e154e1237018bcafea7f37db96cd2902997
Aslak Hollund <aslakhol@gmail.com>
2019-02-07 11:05:37 +0100

    cleanup and get rid of errors

--- frontend/src/__tests__/oneisonetest.js
+++ frontend/src/__tests__/oneisonetest.js
@@ -1,3 +1,6 @@
+// This is just to remove ts complaining about isolated modules
+import * as ts from '../App.tsx'
+
 describe('jest tests', () => {
   it('that one is one', () => {
     expect(1).toEqual(1);
