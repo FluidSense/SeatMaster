7161c7421126fd1ffe7169d4d0078f62674f814c
s0lvang <august.s.solvang@gmail.com>
2019-02-08 21:55:10 +0100

    adds test for applicationController

--- backend/__tests__/conftest.py
+++ backend/__tests__/conftest.py
@@ -3,6 +3,8 @@ import testing.postgresql
 from shared import db
 from flask_sqlalchemy import SQLAlchemy
 from main import create_app
+from controllers.applicationController import application
+from controllers.applicationSeasonController import applicationSeason
 
 
 # Inititiate db with all tables
@@ -21,7 +23,8 @@ def app(database):
     Create a Flask app context for the tests.
     '''
     app = create_app(db_url=database.url())
-
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    register_blueprints(app)
     return app
 
 
@@ -46,3 +49,16 @@ def database(request):
         Postgresql.clear_cache()
 
     return postgres
+
+
+def register_blueprints(app):
+    app.register_blueprint(application)
+    app.register_blueprint(applicationSeason, url_prefix="/season")
+
+
+@pytest.fixture
+def client(app):
+    # app = create_app()
+    register_blueprints(app)
+    client = app.test_client()
+    return client
--- backend/controllers/applicationController.py
+++ backend/controllers/applicationController.py
@@ -8,22 +8,22 @@ application = Blueprint("application", __name__, url_prefix="/application")
 @application.route("/<id>")
 def getApplication(id):
     userApplication = applicationService.getApplicationById(id)
-    return jsonify(userApplication.to_json()) if userApplication else Response("", 200)
+    return jsonify(userApplication.to_json()) if userApplication else Response(json.dumps({}), 200)
 
 
 @application.route("/user/<userid>")
 def getApplicationByUser(userid):
     userApplication = applicationService.getApplicationByUserId(userid)
-    return jsonify(userApplication.to_json()) if userApplication else Response("", 200)
+    return jsonify(userApplication.to_json()) if userApplication else Response(json.dumps({}), 200)
 
 
 @application.route("/registerApplication", methods=["POST"])
-def registerNewApplication():
+def registerApplication():
     if request.is_json:
         form = request.get_json()
         username = form.get("username")
         infoText = form.get("infoText")
         partnerUsername = form.get("partnerUsername")
         responseText, successCode = applicationService.registerApplication(infoText, username, partnerUsername)
-        return Response(json.dumps(form), successCode)
+        return jsonify(json.dumps(form), successCode)
     return abort(400)
