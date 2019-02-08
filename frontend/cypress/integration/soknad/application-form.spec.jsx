72543db72b178dcb5d53608bbe952206523eec86
adrianht <adrianhthompson@gmail.com>
2019-02-08 15:00:22 +0100

    tests some of the functionality

--- frontend/cypress/integration/soknad/application-form.spec.jsx
+++ frontend/cypress/integration/soknad/application-form.spec.jsx
@@ -11,27 +11,44 @@ describe("Tests the application form for user", () => {
     })
 
     it("Greets with my application", () => {
-
+        cy.contains("h1", "My application")
     })
 
     it('Finds name', () => {
+        cy
+        .contains("#fullname")
+        .should("not.be.empty")
         
     })
 
     it('Finds Email', () => {
-       
+        cy
+        .get("#email")
+        .should("be.disabled")
     })
 
     it('Finds Phone', () => {
+        cy
+        .get("#phone")
+        .should("be.disabled")
         
     })
 
     it('Finds Status', () => {
-        
+        cy
+        .get("#status")
+        .should("be.disabled")
+        .should("have.length", 1)
     })
 
-    it('Checks partner name if box is checked', () => {
-        
+    it('Checks partner name if box is checked and inputfield opens', () => {
+        cy
+        .get("#partnerUsername").should("be.disabled")
+
+        .get(".skjemaelement.skjemaelement--horisontal").contains("I wish to sit with my partner").click()
+        .get(".skjemaelement__input.checkboks").should("be.checked")
+
+        .get("#partnerUsername").should("not.be.disabled")
     })
 
     it('Finds keep seat', () => {
@@ -43,6 +60,7 @@ describe("Tests the application form for user", () => {
     })
 
     it('links correctly', () => {
-
+        cy
+        .contains()
     })
 })
