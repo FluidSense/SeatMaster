1b9f4dbb6d47e6eaf1bd184fdc0bc9f262ed5996
adrianht <adrianhthompson@gmail.com>
2019-02-08 13:07:57 +0100

    test boilerplate for application form

---
+++ frontend/cypress/integration/soknad/application-form.spec.jsx
@@ -0,0 +1,48 @@
+describe("Tests the application form for user", () => {
+
+    /*
+    When removing the test info, 
+    we need to seed the db with data in this test for it to pass
+
+    */
+
+    beforeEach(() => {
+        cy.visit("localhost:3000/application")
+    })
+
+    it("Greets with my application", () => {
+
+    })
+
+    it('Finds name', () => {
+        
+    })
+
+    it('Finds Email', () => {
+       
+    })
+
+    it('Finds Phone', () => {
+        
+    })
+
+    it('Finds Status', () => {
+        
+    })
+
+    it('Checks partner name if box is checked', () => {
+        
+    })
+
+    it('Finds keep seat', () => {
+        
+    })
+
+    it('Finds needs box and text if checked', () => {
+        
+    })
+
+    it('links correctly', () => {
+
+    })
+})
