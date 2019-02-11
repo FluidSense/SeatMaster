c37639f88fbbfa2e3d994319fbce95607b93abdf
Aslak Hollund <aslakhol@gmail.com>
2019-02-11 18:37:31 +0100

    add ignore-styles for css mocking

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -2,3 +2,6 @@ import Enzyme from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
 Enzyme.configure({ adapter: new Adapter() });
+
+import register from 'ignore-styles';
+register(['.css', '.sass', '.scss']);
