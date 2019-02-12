708cb49446e335aa477da8cfbb66311b4a6a17d8
Paalar <paaledward@gmail.com>
2019-02-12 18:17:41 +0100

    Fix filldatabase

--- backend/filldatabase.py
+++ backend/filldatabase.py
@@ -1,32 +1,32 @@
-from models import ApplicationSeason, User, Application
+from models.application import Application
+from models.applicationSeason import ApplicationSeason
+from models.user import User
 from services import applicationService
-from database import Session, engine, Base
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta 
+from shared import db
+from main import app
 
-Base.metadata.create_all(engine)
+with app.app_context():
+    the_annoying_user = User("usrnam")
+    the_annoyinger_user = User("putnam")
+    db.session.add(the_annoying_user)
+    db.session.add(the_annoyinger_user)
+    db.session.commit()
+    test_user = db.session.query(User).filter_by(username="usrnam").first()
 
-session = Session()
+    starttime = datetime.now() + timedelta(days=+5)
+    endtime = starttime + timedelta(days=+150)
+    acceptstart = starttime
+    acceptend = starttime + timedelta(days=+7)
 
-the_annoying_user = User("usrnam")
-the_annoyinger_user = User("putnam")
-session.add(the_annoying_user)
-session.add(the_annoyinger_user)
-session.commit()
-test_user = session.query(User).filter_by(username="usrnam").first()
+    some_applicationseason = ApplicationSeason(starttime, endtime, acceptstart, acceptend)
 
-starttime = datetime.now() + timedelta(days=+5)
-endtime = starttime + timedelta(days=+150)
-acceptstart = starttime
-acceptend = starttime + timedelta(days=+7)
+    application1 = applicationService.registerApplication(
+        "sadsda", the_annoying_user.username, the_annoyinger_user.username)
+    application2 = applicationService.registerApplication(
+        "sadsda", the_annoyinger_user.username, the_annoying_user.username)
 
-some_applicationseason = ApplicationSeason(starttime, endtime, acceptstart, acceptend)
-
-application1 = applicationService.registerApplication(
-    "status", "sadsda", the_annoying_user.username, the_annoyinger_user.username)
-application2 = applicationService.registerApplication(
-    "status", "sadsda", the_annoyinger_user.username, the_annoying_user.username)
-
-session.add(some_applicationseason)
-session.commit()
-print("All applications users: ", [u.user for u in session.query(Application).all()])
-session.close()
+    db.session.add(some_applicationseason)
+    db.session.commit()
+    print("All applications users: ", [u.user for u in db.session.query(Application).all()])
+    db.session.close()
