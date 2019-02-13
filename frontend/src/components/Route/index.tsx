e4f8a1e1b6bdcd42a01338fed1ce4af4136b3fd5
adrianht <adrianhthompson@gmail.com>
2019-02-13 11:30:51 +0100

    resolve Route

---
+++ frontend/src/components/Route/index.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import { Route } from 'react-router-dom';
+import ApplicationComponent from '../Application/ApplicationComponent';
+import { HeaderComponent } from '../Header/HeaderComponent';
+import HomeComponent from '../Home/HomeComponent';
+
+export const Routing = () => {
+  return (
+    <div>
+        <HeaderComponent />
+        <Route path="/home" component={HomeComponent} />
+        <Route path="/application" component={ApplicationComponent} />
+    </div>
+  );
+};
