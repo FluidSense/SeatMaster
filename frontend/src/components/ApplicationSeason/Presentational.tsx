162607b3dfef2a725f33c2cab2bfd26918d82bdd
Aslak Hollund <aslakhol@gmail.com>
2019-02-13 20:55:49 +0100

    rework how Etikett in AppStatus is created

--- frontend/src/components/ApplicationSeason/Presentational.tsx
+++ frontend/src/components/ApplicationSeason/Presentational.tsx
@@ -1,15 +1,17 @@
 import EtikettBase from 'nav-frontend-etiketter';
 import React from 'react';
-import { ETIKETT_WARNING } from '../commonConstants';
+import { ETIKETT_INFO } from '../commonConstants';
 import { IApplicationSeason } from './reducer';
 import {
   _SEASON_CLOSED,
   _SEASON_END,
+  _SEASON_OPEN,
   _SEASON_START,
 } from './strings';
 
 interface IApplicationSeasonProps {
   applicationSeason: IApplicationSeason;
+  currentTime: Date;
 }
 
 type validPreText = typeof _SEASON_START | typeof _SEASON_END;
@@ -18,15 +20,7 @@ const formatSeasonString = (preText: validPreText, time: Date) => {
   const day = time.getDate();
   const month = time.getMonth() + 1;
   const year = time.getFullYear();
-  return `${preText} ${day}/${month}/${year}`;
-};
-
-const NavEtikett = (seasonStatus: string) => {
-  return (
-    <EtikettBase type={ETIKETT_WARNING}>
-      {seasonStatus}
-    </EtikettBase>
-  );
+  return `${preText} - ${day}/${month}/${year}`;
 };
 
 const Presentational: React.FunctionComponent<IApplicationSeasonProps> = (props) => {
@@ -34,26 +28,26 @@ const Presentational: React.FunctionComponent<IApplicationSeasonProps> = (props)
   const { applicationPeriodEnd, applicationPeriodStart } = props.applicationSeason;
   const currentTime = new Date();
 
-  let t: any = '';
-  if (currentTime < applicationPeriodEnd) {
-    t = NavEtikett(formatSeasonString(_SEASON_END, applicationPeriodEnd));
+  let t: string = '';
+  if (currentTime < applicationPeriodStart) {
+    t = formatSeasonString(_SEASON_START, applicationPeriodStart);
   } else {
-    t = NavEtikett(_SEASON_CLOSED);
+    t = _SEASON_OPEN;
   }
-  const seasonEndComponent: JSX.Element = t;
+  const seasonStartText: string = t;
 
   let p: any = '';
-  if (currentTime < applicationPeriodStart) {
-    p = NavEtikett(formatSeasonString(_SEASON_START, applicationPeriodStart));
+  if (currentTime < applicationPeriodEnd) {
+    p = formatSeasonString(_SEASON_END, applicationPeriodEnd);
   } else {
-    p = null;
+    p = _SEASON_CLOSED;
   }
-  const seasonStartComponent: JSX.Element = p;
+  const seasonEndText: string = p;
 
   return (
     <>
-      {seasonStartComponent}
-      {seasonEndComponent}
+      <EtikettBase type={ETIKETT_INFO}>{seasonStartText}</EtikettBase>
+      <EtikettBase type={ETIKETT_INFO}>{seasonEndText}</EtikettBase>
     </>
   );
 };
