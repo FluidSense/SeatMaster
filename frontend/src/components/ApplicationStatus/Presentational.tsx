159be4e5bd69185d29d9e1096443f839d9d7c27b
Aslak Hollund <aslakhol@gmail.com>
2019-02-13 17:08:16 +0100

    refactor applicationStatus

--- frontend/src/components/ApplicationStatus/Presentational.tsx
+++ frontend/src/components/ApplicationStatus/Presentational.tsx
@@ -1,21 +1,17 @@
-import React from 'react';
-import { ETIKETT_SUCCESS, ETIKETT_WARNING } from '../commonConstants';
-import { APP_BEING_PROCESSED } from '../Login/constants';
-import { _STATUS_NONE, _STATUS_WAITING } from './strings';
-
 import EtikettBase from 'nav-frontend-etiketter';
+import React from 'react';
 
 interface IApplicationStatusProps {
-  applicationStatus: string;
+  etikettType: 'suksess' | 'info' | 'advarsel' | 'fokus';
+  etikettText: string;
 }
 
 const Presentational: React.FunctionComponent<IApplicationStatusProps> = (props) => {
-  const { applicationStatus } = props;
-  const statusString = applicationStatus === APP_BEING_PROCESSED ? _STATUS_WAITING : _STATUS_NONE;
-  const etikettType = applicationStatus === APP_BEING_PROCESSED ? ETIKETT_SUCCESS : ETIKETT_WARNING;
+  const { etikettType, etikettText } = props;
+
   return (
     <EtikettBase type={etikettType} >
-      {statusString}
+      {etikettText}
     </EtikettBase>
   );
 };
--- frontend/src/components/ApplicationStatus/Strings.tsx
+++ frontend/src/components/ApplicationStatus/Strings.tsx
@@ -1,2 +1,4 @@
-export const _STATUS_WAITING = 'On the waiting list.';
-export const _STATUS_NONE = 'You have not submitted an application.';
+export const _APP_NOT_FOUND = 'We can\'t find an application linked to you';
+export const _APP_SUBMITTED = 'You have submitted and account, it is currently being processed';
+export const _APP_DENIED = 'Your application has been denied.';
+export const _APP_APPROVED = 'Your application has been accepted';
