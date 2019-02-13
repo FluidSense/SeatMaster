9831c179c5f97821751ea8f2a153686114032fe8
adrianht <adrianhthompson@gmail.com>
2019-02-13 16:35:27 +0100

    changed to localhost

--- frontend/src/components/ApplicationForm/Strings.tsx
+++ frontend/src/components/ApplicationForm/Strings.tsx
@@ -1,5 +1,5 @@
 export const _ALERT_USER_SUBMISSION = 'Your application has been submitted ' +
- 'and will be processed. You can see its status on the home page.';
+  'and will be processed. You can see its status on the home page.';
 export const _ALERT_USER_ERROR = 'An error occoured while submitting your application. ' +
   'Please try again later.';
-export const POST_FORM_DATA = 'http://192.168.99.100:5000/application/registerApplication';
+export const POST_FORM_DATA = 'http://localhost:5000/application/registerApplication';
