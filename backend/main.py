5f96cae8ea61c84b5d13d5dcdc6a9f90d0fb96d8
FluidSense <johanneskv96@gmail.com>
2019-02-13 14:12:23 +0100

    remove showcase

--- backend/main.py
+++ backend/main.py
@@ -5,7 +5,6 @@ from flask import Flask, Response, request, abort
 from shared import db
 from flask_migrate import Migrate
 from flask_cors import CORS
-from models.showcase import Showcase
 from controllers.applicationController import application
 from controllers.applicationSeasonController import applicationSeason
 from controllers.roomController import room
@@ -45,48 +44,6 @@ def register_blueprints(app):
 
 
 app = create_app()
-
-
-@app.route("/", methods=["GET"])
-def display():
-    result = db.session.query(Showcase).all()
-    return ", ".join(str(e.id) for e in result)
-
-
-@app.route("/post", methods=["POST"])
-def reveerb():
-    if request.form:
-        thing = Showcase(name=request.form.get("name"))
-        db.session.add(thing)
-        db.session.commit()
-        return Response(request.form, status=201)
-
-
-@app.route("/update", methods=["PUT"])
-def skateboards():
-    if request.data:
-        data = json.loads(request.data)
-        if not data.get("id") or not data.get("name"):
-            return abort(400)
-        dbthing = db.session.query(Showcase).get(data["id"])
-        dbthing.name = data["name"]
-        db.session.add(dbthing)
-        db.session.commit()
-        return Response("OK", status=202)
-
-
-@app.route("/delete", methods=["DELETE"])
-def tea():
-    if request.data:
-        data = json.loads(request.data)
-        if not data.get("name"):
-            return abort(400)
-        dbthing = db.session.query(Showcase).filter_by(name=data["name"]).first()
-        if not dbthing:
-            return abort(404)
-        db.session.delete(dbthing)
-        db.session.commit()
-        return Response("DELETED", status=202)
 
 
 @app.route("/copypasta", methods=["GET"])
