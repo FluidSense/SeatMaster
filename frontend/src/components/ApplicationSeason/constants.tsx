eade644d040812f64468527b4d418c08428a5f50
Aslak Hollund <aslakhol@gmail.com>
2019-02-13 13:38:03 +0100

    refactor url base for get season

--- frontend/src/components/ApplicationSeason/constants.tsx
+++ frontend/src/components/ApplicationSeason/constants.tsx
@@ -2,4 +2,5 @@
 
 // Actions
 export const SET_APPLICATION_SEASON = 'SET_APPLICATION_SEASON';
-export const GET_APPLICATION_SEASON = 'http://localhost:5000/season/getSeason';
+
+export const SEASON_API_URL = 'http://localhost:5000/season/';
