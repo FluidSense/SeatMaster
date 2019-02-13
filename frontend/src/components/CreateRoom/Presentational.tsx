da70d6abf14ed744c89c31ced26bc0000310e825
Paalar <paaledward@gmail.com>
2019-02-13 18:26:44 +0100

    Add basic input functionality

--- frontend/src/components/CreateRoom/Presentational.tsx
+++ frontend/src/components/CreateRoom/Presentational.tsx
@@ -1,9 +1,49 @@
+import HovedKnapp from 'nav-frontend-knapper';
+import { Input, Textarea } from 'nav-frontend-skjema';
 import React from 'react';
+import { validValues } from './index';
+import {
+  _BUTTON_CREATE_ROOM,
+  _INPUT_LABEL_NAME,
+  _INPUT_LABEL_NOTES,
+  _INPUT_LABEL_SEATS,
+} from './strings';
 
-const Presentational: React.FunctionComponent = (props) => {
+interface IProps {
+  notesValue: string;
+  nameValue: string;
+  seatValue: number;
+  buttonDisabled: boolean;
+  updateState: (key: string, value: validValues) => void;
+}
+
+const Presentational: React.FunctionComponent<IProps> = (props) => {
+  const { nameValue, seatValue, notesValue, buttonDisabled } = props;
   return (
-    <h1>whaddup</h1>
+    <>
+      <h1>Create new room</h1>
+      <Input
+        value={nameValue}
+        label={_INPUT_LABEL_NAME}
+        bredde={'XL'}
+      />
+      <Input
+        value={seatValue}
+        label={_INPUT_LABEL_SEATS}
+        bredde={'XS'}
+      />
+      <Textarea
+        value={notesValue}
+        label={_INPUT_LABEL_NOTES}
+        onChange={log}
+      />
+      <HovedKnapp type="hoved" disabled={buttonDisabled}>
+        {_BUTTON_CREATE_ROOM}
+      </HovedKnapp>
+    </>
   );
 };
+
+const log = (e: any) => console.log(e.target.value);
 
 export default Presentational;
--- frontend/src/components/CreateRoom/index.tsx
+++ frontend/src/components/CreateRoom/index.tsx
@@ -3,16 +3,41 @@ import { connect } from 'react-redux';
 import Presentational from './Presentational';
 
 interface IState {
-  something?: any;
+  notesValue: string;
+  nameValue: string;
+  seatValue: number;
+  buttonDisabled: boolean;
+  [key: string]: validValues;
 }
+
+export type validValues = boolean | string | number;
 
 // tslint:disable-next-line:class-name
 class _CreateRoom extends Component<{}, IState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      buttonDisabled: false,
+      nameValue: '',
+      notesValue: '',
+      seatValue: 0,
+    };
+  }
+
   public render() {
+    const { nameValue, notesValue, seatValue, buttonDisabled } = this.state;
     return (
-      <Presentational />
+      <Presentational
+        nameValue={nameValue}
+        notesValue={notesValue}
+        seatValue={seatValue}
+        buttonDisabled={buttonDisabled}
+        updateState={this.updateState}
+      />
     );
   }
+
+  private updateState = (key: string, value: validValues) => this.setState({ [key]: value });
 }
 
 const CreateRoom = connect(
