2f006b22ff2ec75121d9f776484a347896c6978f
FluidSense <johanneskv96@gmail.com>
2019-02-20 15:19:28 +0100

    debug application season actions

--- frontend/src/components/ApplicationSeason/actions.tsx
+++ frontend/src/components/ApplicationSeason/actions.tsx
@@ -5,10 +5,13 @@ import { SEASON_API_URL, SET_APPLICATION_SEASON } from './constants';
 export const fetchApplicationSeasonData = ():
   ThunkAction<Promise<void>, {}, {}, AnyAction> => async (dispatch: Dispatch) => {
     fetch(`${SEASON_API_URL}getSeason`)
+      .then(response => { console.log('I am running!', response); return response; })
       .then(response => response.json())
-      .then(response => !Object.keys(response).length ? Promise.reject() : response)
+      .then(response => { console.log('Now its JSON, right?', response); return response; })
+     // .then(response => !Object.keys(response).length ? Promise.reject() : response)
       .then(result => dispatch({
         payload: result,
         type: SET_APPLICATION_SEASON,
-      }));
+      }))
+      .then(dispatchreturn => { console.log('and this was returned from dispatch?', dispatchreturn); return dispatchreturn; });
   };
