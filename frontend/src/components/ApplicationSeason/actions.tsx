73d36e6fe9f881c0e9f7adb9315397868c5db082
FluidSense <johanneskv96@gmail.com>
2019-02-21 17:54:35 +0100

    lint

--- frontend/src/components/ApplicationSeason/actions.tsx
+++ frontend/src/components/ApplicationSeason/actions.tsx
@@ -2,7 +2,7 @@ import { AnyAction, Dispatch } from 'redux';
 import { ThunkAction } from 'redux-thunk';
 import { SEASON_API_URL, SET_APPLICATION_SEASON } from './constants';
 
-const setApplicationSeason = (payload: object) => ({payload, type: SET_APPLICATION_SEASON });
+const setApplicationSeason = (payload: object) => ({ payload, type: SET_APPLICATION_SEASON });
 
 export const fetchApplicationSeasonData = ():
   ThunkAction<Promise<AnyAction>, {}, {}, AnyAction> => async (dispatch: Dispatch) => {
