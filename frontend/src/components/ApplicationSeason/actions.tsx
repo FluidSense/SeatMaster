c0a81791f2725bb07df8009b0d39c3a6985b25ba
FluidSense <johanneskv96@gmail.com>
2019-02-21 17:49:26 +0100

    change expected type of thunkaction return to AnyAction instead of any

--- frontend/src/components/ApplicationSeason/actions.tsx
+++ frontend/src/components/ApplicationSeason/actions.tsx
@@ -2,13 +2,12 @@ import { AnyAction, Dispatch } from 'redux';
 import { ThunkAction } from 'redux-thunk';
 import { SEASON_API_URL, SET_APPLICATION_SEASON } from './constants';
 
+const setApplicationSeason = (payload: object) => ({payload, type: SET_APPLICATION_SEASON });
+
 export const fetchApplicationSeasonData = ():
-  ThunkAction<Promise<any>, {}, {}, AnyAction> => async (dispatch: Dispatch) => {
+  ThunkAction<Promise<AnyAction>, {}, {}, AnyAction> => async (dispatch: Dispatch) => {
     return fetch(`${SEASON_API_URL}getSeason`)
       .then(response => response.json())
       .then(response => !Object.keys(response).length ? Promise.reject() : response)
-      .then(result => dispatch({
-        payload: result,
-        type: SET_APPLICATION_SEASON,
-      }));
+      .then(result => dispatch(setApplicationSeason(result)));
   };
