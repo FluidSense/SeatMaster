7a717e2d7db7380cea0e7f6dbc3b8f4b94b526fc
Peter Rydberg <hmp.rydberg@gmail.com>
2019-02-22 11:51:21 +0100

    Added logout

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,15 +1,26 @@
 import KnappBase from 'nav-frontend-knapper';
 import * as React from 'react';
-import { Link } from 'react-router-dom';
 import './header.css';
+import userManager from './../../utils/userManager';
 
 export const Header: React.FunctionComponent = (props) => {
+  const doLogout = (event: React.MouseEvent) => {
+    event.preventDefault();
+    userManager.signoutRedirect();
+  };
+  const onClickAction = (event: React.MouseEvent) => doLogout(event);
+
   return (
     <div id="student-header">
       You're logged in as Christoffer Lofsberg
-      <Link to="/login">
-        <KnappBase type="hoved" htmlType="button" id="logoutButton">Log Out</KnappBase>
-      </Link>
+      <KnappBase
+        type="hoved"
+        htmlType="button"
+        id="logoutButton"
+        onClick={onClickAction}
+      >
+      Log Out
+      </KnappBase>
     </div>
   );
 };
