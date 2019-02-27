272b9539a8bd15f628b052a99a10fd40b2bfe648
Paalar <paaledward@gmail.com>
2019-02-28 00:34:31 +0100

    rework link creator

--- frontend/src/components/ViewRooms/RoomLink.tsx
+++ frontend/src/components/ViewRooms/RoomLink.tsx
@@ -1,7 +1,7 @@
 import { LenkepanelBase } from 'nav-frontend-lenkepanel';
 import { Element } from 'nav-frontend-typografi';
 import React from 'react';
-import { Link, Route } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { IRoom } from './index';
 import { _LINK_NAME_TITLE, _LINK_NOTES_TITLE } from './strings';
 
@@ -9,26 +9,30 @@ interface IProps {
   room: IRoom;
 }
 
-const noUnderline = { textDecoration: 'none' };
-
 const RoomLink: React.FunctionComponent<IProps> = (props) => {
   const { room } = props;
-  return (
-    <Link
+  const link = (panelProps: any) => (
+    <NavLink
       to={{ pathname: '/admin/rooms/update-room', room: { ...room } }}
-      style={noUnderline}
+      className="lenkepanel lenkepanel--border"
     >
-      <LenkepanelBase key={room.id}>
-        <div className="link-name">
-          <Element>{_LINK_NAME_TITLE}</Element>
-          {room.name}
-        </div>
-        <div className="link-notes">
-          <Element>{_LINK_NOTES_TITLE}</Element>
-          {room.info}
-        </div>
-      </LenkepanelBase>
-    </Link>
+      {panelProps.children}
+    </NavLink>);
+  return (
+    <LenkepanelBase
+      key={room.id}
+      linkCreator={link}
+      href={'/admin/rooms/update-room'}
+    >
+      <div className="link-name">
+        <Element>{_LINK_NAME_TITLE}</Element>
+        {room.name}
+      </div>
+      <div className="link-notes">
+        <Element>{_LINK_NOTES_TITLE}</Element>
+        {room.info}
+      </div>
+    </LenkepanelBase>
   );
 };
 
