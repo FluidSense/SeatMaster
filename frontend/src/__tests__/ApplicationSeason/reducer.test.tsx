64ae6e1033dbdf7441c7fe71e8250ef5fffb54d5
FluidSense <johanneskv96@gmail.com>
2019-02-27 15:22:47 +0100

    fix test to match new structure

--- frontend/src/__tests__/ApplicationSeason/reducer.test.tsx
+++ frontend/src/__tests__/ApplicationSeason/reducer.test.tsx
@@ -6,10 +6,12 @@ test('reducers', () => {
   state = reducers(
     {
       applicationSeason:{
-        applicationPeriodEnd: moment('1970-01-01T00:00:01.970Z'),
-        applicationPeriodStart: moment('1970-01-01T00:00:01.970Z'),
-        end: moment('1970-01-01T00:00:01.970Z'),
-        start: moment('1970-01-01T00:00:01.970Z'),
+        currentSeason: {
+          applicationPeriodEnd: moment('1970-01-01T00:00:01.970Z'),
+          applicationPeriodStart: moment('1970-01-01T00:00:01.970Z'),
+          end: moment('1970-01-01T00:00:01.970Z'),
+          start: moment('1970-01-01T00:00:01.970Z'),
+        },
       },
       userInformation:{
         applicationStatus:'NOT_FOUND',
@@ -34,10 +36,12 @@ test('reducers', () => {
     });
   expect(state).toEqual({
     applicationSeason:{
-      applicationPeriodEnd: moment('2019-03-03 11:41:04.793276'),
-      applicationPeriodStart: moment('2019-02-24 11:41:04.793276'),
-      end: moment('2019-07-24 11:41:04.793276'),
-      start: moment('2019-02-24 11:41:04.793276'),
+      currentSeason: {
+        applicationPeriodEnd: moment('2019-03-03 11:41:04.793276'),
+        applicationPeriodStart: moment('2019-02-24 11:41:04.793276'),
+        end: moment('2019-07-24 11:41:04.793276'),
+        start: moment('2019-02-24 11:41:04.793276'),
+      },
     },
     userInformation: {
       applicationStatus:'NOT_FOUND',
--- frontend/src/__tests__/CreateSeason/index.test.tsx
+++ frontend/src/__tests__/CreateSeason/index.test.tsx
@@ -1,29 +1,46 @@
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import fetchMock from 'fetch-mock';
 import moment from 'moment';
 import * as React from 'react';
+import { Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
 import { POST_NEW_SEASON_URL } from '../../components/commonConstants';
 import CreateSeason, { format, IState, setTime } from '../../components/CreateSeason/index';
+import Presentational from '../../components/CreateSeason/Presentational';
 
 fetchMock.mock(POST_NEW_SEASON_URL, 201);
 
 describe('Create season', () => {
+  const middlewares = [thunk];
+  const mockStore = configureMockStore(middlewares);
+  const store = mockStore({ applicationSeason: {} });
   it('renders correctly', () => {
-    const wrapper = shallow(<CreateSeason />);
-    expect(toJson(wrapper)).toMatchSnapshot();
+    Date.now = jest.fn(() => new Date(Date.UTC(2017, 0, 1)).valueOf());
+    const wrapper = mount(
+    <Provider store={store}>
+      <CreateSeason />
+    </Provider>);
+    const season = wrapper.find(CreateSeason);
+
+    expect(toJson(season)).toMatchSnapshot();
   });
 
   it('Check if creating is available', () => {
-    const wrapper = shallow(<CreateSeason />);
-    const state: IState = wrapper.state();
+    const wrapper = mount(
+    <Provider store={store}>
+      <CreateSeason />
+    </Provider>);
+    const component = wrapper.find('_CreateSeason');
+    const state: IState = component.state();
     const body = JSON.stringify({
       newPeriodEnd: state.periodEnd.format(format),
       newPeriodStart: state.periodStart.format(format),
       newRoomEnd: state.roomEnd.format(format),
       newRoomStart: state.roomStart.format(format),
     });
-    const newSeasonButton = wrapper.dive().find('#new-season-btn');
+    const newSeasonButton = wrapper.find('#new-season-btn').hostNodes();
     newSeasonButton.simulate('click');
     const fetchOptions = fetchMock.lastOptions();
     expect(fetchOptions.body).toEqual(body);
