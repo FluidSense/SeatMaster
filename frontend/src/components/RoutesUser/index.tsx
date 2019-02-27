07b221c3df8f4427cb227e354442de6958c7530a
Peter Rydberg <hmp.rydberg@gmail.com>
2019-02-27 17:09:00 +0100

    Lint

--- frontend/src/components/RoutesUser/index.tsx
+++ frontend/src/components/RoutesUser/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Route, Switch, Redirect } from 'react-router-dom';
+import { Redirect, Route, Switch } from 'react-router-dom';
 import Application from '../Application/index';
 import { Header } from '../Header';
 import Home from '../Home';
