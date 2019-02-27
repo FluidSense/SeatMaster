eb259ff566d942ef972a2e88b3b6768a339c7311
Paalar <paaledward@gmail.com>
2019-02-27 13:53:44 +0100

    Rename files

--- frontend/src/components/ViewRooms/actions.ts
+++ frontend/src/components/ViewRooms/actions.ts
@@ -4,7 +4,6 @@ import { getAllRooms } from '../../API/calls';
 import { FETCH_ROOMS } from './constants';
 import { IRoom } from './index';
 
-/*
 const retrievedRooms = (payload: IRoom[]) => ({
   payload,
   type: FETCH_ROOMS,
@@ -14,16 +13,4 @@ export const fetchAllRooms = ():
   ThunkAction<Promise<void>, {}, {}, AnyAction> => async (dispatch: Dispatch) => {
     const result = await getAllRooms();
     if (result) dispatch(retrievedRooms(result));
-  };
-*/
-
-const action = (payload: any) => ({
-  payload,
-  type: 'ACTION',
-});
-
-export const thunkAction = ():
-  ThunkAction<Promise<void>, {}, {}, AnyAction> => async (dispatch: Dispatch) => {
-    const result = await undefined;
-    if (result) dispatch(action(result));
   };
