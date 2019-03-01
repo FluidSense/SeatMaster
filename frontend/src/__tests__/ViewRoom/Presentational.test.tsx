93530ac1a368289c7e686ab182fde0a280d97d86
Paalar <paaledward@gmail.com>
2019-03-01 20:22:18 +0100

    update tests and snapshot

--- frontend/src/__tests__/ViewRoom/Presentational.test.tsx
+++ frontend/src/__tests__/ViewRoom/Presentational.test.tsx
@@ -2,6 +2,7 @@ import { mount, shallow } from 'enzyme';
 import * as React from 'react';
 import { IRoom } from '../../components/ViewRooms/index';
 import Presentational from '../../components/ViewRooms/Presentational';
+import RoomLink from '../../components/ViewRooms/RoomLink';
 
 export const mockRoom = (id: number, name: string, info: string, seat: number) => ({
   id,
@@ -11,18 +12,19 @@ export const mockRoom = (id: number, name: string, info: string, seat: number) =
 });
 
 describe('View Room Presentational', () => {
+  const onClick = () => null;
   it('Renders nothing', () => {
     const mockEmptyList: IRoom[] = [];
-    const wrapper = shallow(<Presentational rooms={mockEmptyList} />);
+    const wrapper = shallow(<Presentational rooms={mockEmptyList} onClick={onClick} />);
     const links = wrapper.getElements()[0];
     expect(links).toEqual(null);
   });
 
   it('Renders one item', () => {
     const room1 = mockRoom(1, 'X-Wing', 'notes', 2);
-    const wrapper = shallow(<Presentational rooms={[room1]} />);
-    const links = wrapper.getElements()[0].props.children.length;
-    expect(links).toEqual(1);
+    const wrapper = shallow(<Presentational rooms={[room1]} onClick={onClick} />);
+    const links = wrapper.find(RoomLink);
+    expect(links.length).toEqual(1);
   });
 
   it('Renders three items', () => {
@@ -30,8 +32,8 @@ describe('View Room Presentational', () => {
     const room2 = mockRoom(2, 'X-Vinge', 'notater', 4);
     const room3 = mockRoom(3, 'X-Wang', 'Nates', 5);
     const mockList = [room1, room2, room3];
-    const wrapper = shallow(<Presentational rooms={mockList} />);
-    const links = wrapper.getElements()[0].props.children.length;
-    expect(links).toEqual(3);
+    const wrapper = shallow(<Presentational rooms={mockList} onClick={onClick} />);
+    const links = wrapper.find(RoomLink);
+    expect(links.length).toEqual(3);
   });
 });
