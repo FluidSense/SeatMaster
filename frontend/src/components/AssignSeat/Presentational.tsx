6916155d5aeaca40f828f6080ac3f8b4ef2a7621
FluidSense <johanneskv96@gmail.com>
2019-03-04 20:46:31 +0100

    add presentational

--- frontend/src/components/AssignSeat/Presentational.tsx
+++ frontend/src/components/AssignSeat/Presentational.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import { IApplication } from '../../API/interfaces';
 
 interface IProps {
+  applications: IApplication[];
 }
 
 const Presentational: React.FunctionComponent<IProps> = (props) => {
-  const { } = props;
-
+  const { applications } = props;
+  if (!applications) return null;
+  if (!applications.length) return null;
+  const applicationList = applications.map(application => (
+    <ApplicationLink key={application.id} application={application} />
+  ));
   return (
-
+    <>
+      {applicationList}
+    </>
   );
 };
 
