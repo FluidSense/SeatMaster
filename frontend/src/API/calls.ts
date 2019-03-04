2a060f114ccd3717d16e8d7ec3d977b93b4b7639
FluidSense <johanneskv96@gmail.com>
2019-03-04 21:33:33 +0100

    lint

--- frontend/src/API/calls.ts
+++ frontend/src/API/calls.ts
@@ -2,7 +2,7 @@ import { IApplicationSeason } from '../components/ApplicationSeason/reducer';
 import { IRoom } from '../components/ViewRooms';
 import { deleteJson, getJson, postJson, putJson } from './callDefinitions';
 import {
-  GET_APPLICATION_BY_USERID_URL, POST_FORM_DATA_URL, ROOM_URL, SEASON_URL, GET_APPLICATIONS_URL,
+  GET_APPLICATION_BY_USERID_URL, GET_APPLICATIONS_URL, POST_FORM_DATA_URL, ROOM_URL, SEASON_URL,
 } from './constants';
 import {
   IApplication,
