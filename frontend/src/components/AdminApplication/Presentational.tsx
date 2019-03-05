77a3a33a778cb24a9591611962e4f8fc3f91bde3
FluidSense <johanneskv96@gmail.com>
2019-03-05 20:58:54 +0100

    init way for admin to see assigned seat

--- frontend/src/components/AdminApplication/Presentational.tsx
+++ frontend/src/components/AdminApplication/Presentational.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { IApplication } from '../Application';
 import ApplicationOverview from '../ApplicationReview/ApplicationOverview';
 import AssignSeat from '../AssignSeat';
+import ApplicationSeatDisplay from './ApplicationSeatDisplay';
 
 interface IProps {
   application?: IApplication;
@@ -13,6 +14,7 @@ const Presentational: React.FunctionComponent<IProps> = (props) => {
   return (
     <div className="main-content">
       <ApplicationOverview application={application} title={application.fullname}/>
+      <ApplicationSeatDisplay />
       <AssignSeat application={application}/>
     </div>
   );
