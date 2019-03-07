a87f21b6f4bc4c784dfc7d5d51af41c72b83c797
Aslak Hollund <aslakhol@gmail.com>
2019-03-07 21:49:03 +0100

    holy shit, it updates now

--- frontend/src/components/Seats/actions.ts
+++ frontend/src/components/Seats/actions.ts
@@ -21,23 +21,23 @@ const seatDeleted = (success: boolean) => ({
   type: DELETE_SEAT,
 });
 
-export const createSeat = (seats: ISeat[], idOfRoom: number):
-  ThunkAction<Promise<void>, {}, {}, AnyAction> => async (dispatch: ThunkDispatch<{}, {}, any>) => {
-    let nextID = 'A1';
-    if (seats.length > 0) {
-      // Splits id in letters and numbers
-      const idParts = seats[seats.length - 1].id.split(/([0-9]+)/);
-      // Creates new id by incrementing the number on the last id
-      nextID = `${idParts[0]}${parseInt(idParts[1], 10) + 1}`;
-    }
+// export const createSeat = (seats: ISeat[], idOfRoom: number):
+//   ThunkAction<Promise<void>, {}, {}, AnyAction> => async (dispatch: ThunkDispatch<{}, {}, any>) => {
+//     let nextID = 'A1';
+//     if (seats.length > 0) {
+//       // Splits id in letters and numbers
+//       const idParts = seats[seats.length - 1].id.split(/([0-9]+)/);
+//       // Creates new id by incrementing the number on the last id
+//       nextID = `${idParts[0]}${parseInt(idParts[1], 10) + 1}`;
+//     }
 
-    const seat: IPostSeat = {
-      id: nextID,
-      info: '',
-      roomId: idOfRoom,
-    };
-    dispatch(createSeatAction(seat));
-  };
+//     const seat: IPostSeat = {
+//       id: nextID,
+//       info: '',
+//       roomId: idOfRoom,
+//     };
+//     dispatch(createSeatAction(seat));
+//   };
 
 export const createSeatAction = (seat: IPostSeat):
   ThunkAction<Promise<void>, {}, {}, AnyAction> => async (dispatch: Dispatch) => {
--- frontend/src/components/Seats/index.tsx
+++ frontend/src/components/Seats/index.tsx
@@ -1,15 +1,36 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { ThunkDispatch } from 'redux-thunk';
 import { IStore } from '../../store';
-import { createSeat } from './actions';
+import { createSeatAction } from './actions';
 import Presentational from './Presentational';
+import { IPostSeat } from '../../API/interfaces';
 
 export interface ISeat {
   id: string;
   info: string;
   roomId: number;
 }
+
+interface IState {
+  roomId: number;
+  seats: ISeat[];
+}
+
+interface IProps {
+  roomId: number;
+  seats: ISeat[];
+}
+
+interface IDispatchProps {
+  createSeat: (data: IPostSeat, roomId: number) => void;
+  createSeatClick: (seats: ISeat[], roomId: number) => void;
+}
+
+interface IStateProps {
+}
+
+type Props = IDispatchProps & IStateProps & IProps;
 
 const editSeatID = () => {
   // edit seat
@@ -19,16 +40,71 @@ const deleteSeat = () => {
   // delete seat
 };
 
-const mapStateToProps = (state: IStore) => ({
+// tslint:disable-next-line:class-name
+class _Container extends Component<Props, IState> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      roomId: -1,
+      seats: [],
+    };
+  }
+
+  public componentDidMount = () => {
+    const { roomId, seats } = this.props;
+    this.setState({ roomId, seats });
+  }
+
+  public render() {
+    const { roomId, seats } = this.state;
+    const { createSeatClick } = this.props;
+    return (
+      <Presentational
+        roomId={roomId}
+        seats={seats}
+        createSeatClick={this.create}
+      />
+    );
+  }
+
+  private create = () => {
+    const { seats, roomId } = this.state;
+    const { createSeat } = this.props;
+    let nextID = 'A1';
+    if (seats.length > 0) {
+      // Splits id in letters and numbers
+      const idParts = seats[seats.length - 1].id.split(/([0-9]+)/);
+      // Creates new id by incrementing the number on the last id
+      nextID = `${idParts[0]}${parseInt(idParts[1], 10) + 1}`;
+    }
+
+    const seat: IPostSeat = {
+      id: nextID,
+      info: '',
+      roomId,
+    };
+
+    this.setState(prevState => ({
+      seats: [
+        ...prevState.seats,
+        seat,
+      ]
+    }))
+
+    createSeat(seat, roomId);
+  }
+}
+
+const mapStateToProps = (state: IStore, ownProps: any) => ({
 });
 
 const mapDispatchToProps = (dispatch: ThunkDispatch<{}, {}, any>, ownProps: any) => ({
-  createSeatClick: (seats: ISeat[], roomId: number) => dispatch(createSeat(seats, roomId)),
+  createSeat: (data: IPostSeat) => dispatch(createSeatAction(data)),
 });
 
 const Container = connect(
   mapStateToProps,
   mapDispatchToProps,
-)(Presentational);
+)(_Container);
 
 export default Container;
