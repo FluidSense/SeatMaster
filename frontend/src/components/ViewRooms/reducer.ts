b5efecd57fa589ad18cb3fec2630785c406114eb
Aslak Hollund <aslakhol@gmail.com>
2019-03-08 11:57:36 +0100

    remove seat from room reducer

--- frontend/src/components/ViewRooms/reducer.ts
+++ frontend/src/components/ViewRooms/reducer.ts
@@ -1,7 +1,7 @@
 import { AnyAction } from 'redux';
 import { CREATE_SEAT } from '../Seats/constants';
 import { FETCH_ROOMS } from './constants';
-import { IRoom, ISeat } from './index';
+import { IRoom } from './index';
 
 export interface IRoomState {
   rooms: IRoom[];
@@ -15,22 +15,10 @@ const reducer = (
   state: IRoomState = initialState,
   action: AnyAction,
 ): IRoomState => {
-  const { type, payload, seat } = action;
+  const { type, payload } = action;
   switch (type) {
     case FETCH_ROOMS:
       return { ...state, rooms: payload };
-    case CREATE_SEAT:
-      const newRooms: IRoom[] = state.rooms.map((thisRoom) => {
-        const returnValue = { ...thisRoom };
-        if (thisRoom.id === 9) {
-          thisRoom.seats.seats = [...thisRoom.seats.seats, seat];
-        }
-        return returnValue;
-      });
-      return {
-        ...state,
-        rooms: newRooms,
-      };
     default:
       return state;
   }
