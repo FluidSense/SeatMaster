77780e8e858dfc2633f7684bb51a8299d63a8c33
Peter Rydberg <hmp.rydberg@gmail.com>
2019-03-19 22:02:20 +0100

    Updated sidebar urls

--- frontend/src/components/SideBar/index.tsx
+++ frontend/src/components/SideBar/index.tsx
@@ -8,28 +8,46 @@ interface IUrl {
   title: string;
 }
 
-const pageTitle = 'Navigation';
+const userTitle = 'User navigation';
+const adminTitle = 'Administrator tools';
 
-const adminUrls: IUrl[] = [
-  { url: '/admin/', title: 'Admin' },
-  { url: '/admin/rooms/', title: 'Rooms' },
-  { url: '/admin/create-season/', title: 'Create Season' },
-  { url: '/admin/applications', title: 'Applications' },
+const userUrls: IUrl[] = [
+  { url: '/', title: 'Home' },
+  { url: '/application/', title: 'Application' },
 ];
 
-const Presentational: React.FunctionComponent<{}> = (props) => {
-  const urls = adminUrls.map(url => (
+const adminUrls: IUrl[] = [
+  { url: '/admin/', title: 'Admin' }, // TODO: Create landing page for admin
+  { url: '/admin/rooms/', title: 'Rooms' },
+  { url: '/admin/create-season/', title: 'Create Season' },
+  { url: '/admin/applications/', title: 'Applications' },
+];
+
+const Presentational: React.FunctionComponent<{}> = () => {
+  const userUrlList = userUrls.map(url => (
+    <li key={userUrls.indexOf(url)}>
+      <NavLink to={url.url} exact={true}>
+        {url.title}
+      </NavLink>
+    </li>));
+
+  const adminUrlList = adminUrls.map(url => (
     <li key={adminUrls.indexOf(url)}>
       <NavLink to={url.url} exact={true}>
         {url.title}
       </NavLink>
     </li>));
+
   return (
-    <div id="admin-side-bar">
+    <div id="side-bar">
       <nav>
-        <Systemtittel>{pageTitle}</Systemtittel>
-        <ul id="nav-list">
-          {urls}
+        <Systemtittel>{userTitle}</Systemtittel>
+        <ul className="nav-list">
+          {userUrlList}
+        </ul>
+        <Systemtittel>{adminTitle}</Systemtittel>
+        <ul className="nav-list">
+          {adminUrlList}
         </ul>
       </nav>
     </div>
