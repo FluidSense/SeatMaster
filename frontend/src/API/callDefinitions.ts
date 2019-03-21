ed53b8815b8763ee793e633ea2119fec07345cd4
Aslak Hollund <aslakhol@gmail.com>
2019-03-21 13:54:57 +0100

    expand delete methods with optional param

--- frontend/src/API/callDefinitions.ts
+++ frontend/src/API/callDefinitions.ts
@@ -12,16 +12,22 @@ const genericFetch = (fetchType: fetchTypes, url: string, data: any) => {
 
 const postFetch = (url: string, data: any) => genericFetch('POST', url, data);
 const putFetch = (url: string, data: any) => genericFetch('PUT', url, data);
-const deleteFetch = (url: string, id: any) => {
-  return fetch(`${url}${id}`, {
-    method: 'DELETE',
-  });
+const deleteFetch = (url: string, id: any, additionalId?: any) => {
+  if (!additionalId) {
+    return fetch(`${url}${id}`, {
+      method: 'DELETE',
+    });
+  } else {
+    return fetch(`${url}${id}/${additionalId}`, {
+      method: 'DELETE',
+    });
+  }
 };
 
 export const postJson = (url: string, data: any) => {
   return postFetch(url, data).then(response => response.ok
-  ? response.json()
-  : false);
+    ? response.json()
+    : false);
 };
 
 export const putJson = (url: string, data: any) => {
@@ -31,11 +37,18 @@ export const putJson = (url: string, data: any) => {
       : false);
 };
 
-export const deleteJson = (url: string, id: any) => {
-  return deleteFetch(url, id)
-    .then(response => response.ok
-      ? response.json()
-      : false);
+export const deleteJson = (url: string, id: any, additionalId?: any) => {
+  if (!additionalId) {
+    return deleteFetch(url, id)
+      .then(response => response.ok
+        ? response.json()
+        : false);
+  } else {
+    return deleteFetch(url, id, additionalId)
+      .then(response => response.ok
+        ? {}
+        : false);
+  }
 };
 
 export const getJson = (url: string) => fetch(url).then(response => response.ok
