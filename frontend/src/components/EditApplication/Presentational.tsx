9864c6ad99e0ba4917b7397d27000c6102e51ac7
FluidSense <22146437+FluidSense@users.noreply.github.com>
2019-03-22 22:21:58 +0100

    change edit application to grid

--- frontend/src/components/EditApplication/Presentational.tsx
+++ frontend/src/components/EditApplication/Presentational.tsx
@@ -2,6 +2,7 @@ import { Input, SkjemaGruppe } from 'nav-frontend-skjema';
 import React from 'react';
 import { boolToString } from '../../utils/typeFormatter';
 import { IApplication } from '../Application';
+import './editApplication.css';
 
 interface IProps {
   applications: IApplication[];
@@ -24,48 +25,51 @@ const Presentational: React.FunctionComponent<IProps> = (props) => {
   const partnerName = partner ? partner.fullname : '';
 
   return (
-    <SkjemaGruppe className="main-content">
-      <Input
-        label="Name"
-        bredde="L"
-        defaultValue={application.user.fullname}
-      />
-      <Input
-        label="Master status"
-        bredde="L"
-        defaultValue={application.user.masterStatus}
-      />
-      <Input
-        label="E-Mail"
-        bredde="L"
-        defaultValue={application.user.email}
-      />
-      <Input
-        label="Partner username"
-        bredde="L"
-        defaultValue={partnerName}
-      />
-      <Input
-        label="Preferred room"
-        bredde="L"
-        defaultValue={application.preferredRoom}
-      />
-      <Input
-        label="Seat Rollover"
-        bredde="L"
-        defaultValue={boolToString(application.seatRollover)}
-      />
-      <Input
-        label="Needs"
-        bredde="L"
-        defaultValue={application.needs}
-      />
-      <Input
-        label="Comments"
-        bredde="L"
-        defaultValue={application.comments}
-      />
-    </SkjemaGruppe>
+    <div className="main-content">
+      <SkjemaGruppe className="edit-application">
+        <Input
+          label="Name"
+          bredde="L"
+          defaultValue={application.user.fullname}
+        />
+        <Input
+          label="Master status"
+          bredde="L"
+          defaultValue={application.user.masterStatus}
+        />
+        <Input
+          label="E-Mail"
+          bredde="L"
+          defaultValue={application.user.email}
+        />
+        <Input
+          label="Partner username"
+          bredde="L"
+          defaultValue={partnerName}
+        />
+        <Input
+          label="Preferred room"
+          bredde="L"
+          defaultValue={application.preferredRoom}
+        />
+        <Input
+          label="Seat Rollover"
+          bredde="L"
+          defaultValue={boolToString(application.seatRollover)}
+        />
+        <Input
+          label="Needs"
+          bredde="L"
+          id="edit-needs"
+          defaultValue={application.needs}
+        />
+        <Input
+          label="Comments"
+          bredde="L"
+          defaultValue={application.comments}
+        />
+      </SkjemaGruppe>
+    </div>
   );
 };
 
