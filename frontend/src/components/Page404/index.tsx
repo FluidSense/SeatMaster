ca3213021190a1af2f1277da1411c50170f59717
Peter Rydberg <hmp.rydberg@gmail.com>
2019-03-22 13:20:18 +0100

    Made veileder match the one in register user

--- frontend/src/components/Page404/index.tsx
+++ frontend/src/components/Page404/index.tsx
@@ -1,28 +1,33 @@
-import Person from '-!svg-react-loader!./../../assets/veileder.svg';
 import KnappBase from 'nav-frontend-knapper';
-import Veileder from 'nav-frontend-veileder';
+import { Sidetittel } from 'nav-frontend-typografi';
+import Veilederpanel from 'nav-frontend-veilederpanel';
 import * as React from 'react';
 import { Link } from 'react-router-dom';
+import veileder from './../RegisterUser/veileder';
+import './page404.css';
 
 const Page404: React.FunctionComponent = () => {
 
   const _VELEDER_TEXT = 'There does not seem to be a page here. Click the button below to go back.';
 
   return (
-    <div>
-      <Veileder storrelse="XL" tekst={_VELEDER_TEXT}>
-        <Person />
-      </Veileder>
-
-      <Link to="/" >
-        <KnappBase
-          type="hoved"
-          htmlType="button"
-          id="backButton"
-        >
-        Go to home
-        </KnappBase>
-      </Link>
+    <div className="main-content veileder-wrapper">
+      <Veilederpanel type={'plakat'} kompakt={true} svg={veileder}>
+        <div className="contents">
+          <Sidetittel tag="h1">404</Sidetittel>
+          {_VELEDER_TEXT}
+          <br />
+          <Link to="/" >
+            <KnappBase
+              type="hoved"
+              htmlType="button"
+              id="backButton"
+            >
+              Go to home
+            </KnappBase>
+          </Link>
+        </div>
+      </Veilederpanel>
     </div>
   );
 };
