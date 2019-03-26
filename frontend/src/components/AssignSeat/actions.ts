1daabb9dfa4f059d852586ca9e0598a91a1dc3b3
Paalar <paaledward@gmail.com>
2019-03-26 13:41:39 +0100

    update assign seat to delete student from seat

--- frontend/src/components/AssignSeat/actions.ts
+++ frontend/src/components/AssignSeat/actions.ts
@@ -1,13 +1,15 @@
 import { AnyAction, Dispatch } from 'redux';
 import { ThunkAction } from 'redux-thunk';
-import { putUserOnSeat } from '../../API/calls';
+import { putUserOnSeat, removeStudentFromSeat } from '../../API/calls';
 import { IUser } from '../../API/interfaces';
 import { IRoom, ISeat } from '../ViewRooms';
-import { SUCCESSFULL_SEAT_ASSIGNMENT } from './constants';
+import { REMOVE_STUDENT_SUCCESS, SUCCESSFULL_SEAT_ASSIGNMENT } from './constants';
 
 const successfullyAssignedSeat = () => ({
   type: SUCCESSFULL_SEAT_ASSIGNMENT,
 });
+
+const successfullyRemovedStudent = () => ({ type: REMOVE_STUDENT_SUCCESS });
 
 export const assignUserToSeat = (user: IUser, seat: ISeat):
   ThunkAction<Promise<void>, {}, {}, AnyAction> => async (dispatch: Dispatch) => {
@@ -17,4 +19,11 @@ export const assignUserToSeat = (user: IUser, seat: ISeat):
       userId: user.id,
     });
     if (response) dispatch(successfullyAssignedSeat());
+  };
+
+export const removeStudent = (roomId: number, seatId: string):
+  ThunkAction<Promise<void>, {}, {}, AnyAction> => async (dispatch: Dispatch) => {
+    const data = { roomId, seatId };
+    const response = await removeStudentFromSeat(data);
+    if (response) dispatch(successfullyRemovedStudent());
   };
