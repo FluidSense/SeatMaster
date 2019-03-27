60e2018633afdc5fbeee57b24dc80f809973772d
FluidSense <22146437+FluidSense@users.noreply.github.com>
2019-03-27 17:59:44 +0100

    activate assign if selected application

--- frontend/src/components/AdminViewRoom/SeatPlacer.tsx
+++ frontend/src/components/AdminViewRoom/SeatPlacer.tsx
@@ -2,7 +2,6 @@ import KnappBase from 'nav-frontend-knapper';
 import PanelBase, { Panel } from 'nav-frontend-paneler';
 import { Select } from 'nav-frontend-skjema';
 import React from 'react';
-import { IUser } from '../../API/interfaces';
 import { IApplication } from '../Application';
 import { ISeat } from '../ViewRooms';
 
@@ -12,24 +11,19 @@ interface IProps {
 }
 
 interface IState {
-  selectedUser?: IUser;
+  selectedApp?: IApplication;
 }
 
 class SeatPlacer extends React.Component<IProps, IState> {
   constructor(props: IProps) {
     super(props);
     this.state = {
-      selectedUser: undefined,
+      selectedApp: undefined,
     };
   }
   public render() {
     const { seat, applications } = this.props;
-    const { selectedUser } = this.state;
-    if (!applications) return null;
-    const appsWithUsers = applications.filter(application => application.user ? true : false);
-    const usersWithUndefined = appsWithUsers.map(application => application.user);
-    const users = usersWithUndefined.filter(user => user !== undefined);
-    console.log('users:', users);
+    const { selectedApp } = this.state;
 
     if (seat.user) {
       return (
@@ -40,22 +34,34 @@ class SeatPlacer extends React.Component<IProps, IState> {
         </Panel>
       );
     }
+    if (!applications) return null;
+    const userOptions = this.applicationsToOption(applications);
     return (
       <Panel border={true} style={{ display:'flex', width:'35%' }}>
         <PanelBase border={true}>{seat.id}</PanelBase>
-        <Select label="">
+        <Select label="" onChangeCapture={this.selectApp}>
           <option value="">Select user</option>
+          {userOptions}
         </Select>
-        <KnappBase type="hoved" disabled={selectedUser ? false : true}>
+        <KnappBase type="hoved" disabled={selectedApp ? false : true}>
           Assign Seat
         </KnappBase>
       </Panel>
     );
   }
 
-  private usersToOption = (users: IUser[]) => users.map((user, index) => {
-    return <option key={index} value={user.id}>{user.username}</option>;
+  private applicationsToOption = (apps: IApplication[]) => apps.map((app, index) => {
+    return <option key={index} value={app.id}>{app.user.username}</option>;
   })
+
+  private selectApp = (e: React.FormEvent) => {
+    const field = e.target as HTMLFormElement;
+    const applicationId = parseInt(field.value, 10);
+    const applications = this.props.applications ? this.props.applications : [];
+    const application = applications.find(app => app.id === applicationId);
+    this.setState({ selectedApp: application });
+  }
+
 }
 
 export default SeatPlacer;
