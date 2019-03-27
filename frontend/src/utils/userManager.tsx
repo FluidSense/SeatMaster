89cbc6d5abc8802e222afd78434b577bca8c7ea3
Peter Rydberg <hmp.rydberg@gmail.com>
2019-03-27 17:40:52 +0100

    Removed longterm scope

--- frontend/src/utils/userManager.tsx
+++ frontend/src/utils/userManager.tsx
@@ -5,18 +5,13 @@ import { DATAPORTEN_CLIENT_ID } from '../config';
 // Configuration for dataporten with the properties it need.
 const userManagerConfig = {
   authority: 'https://auth.dataporten.no',
-  automaticSilentRenew: false,
   client_id: DATAPORTEN_CLIENT_ID,
   filterProtocolClaims: true,
   loadUserInfo: true,
   redirect_uri: `${window.location.protocol}//` +
     `${window.location.hostname}${window.location.port ? `:${window.location.port}` : ''}/callback`,
   response_type: 'code',
-  scope: 'email openid profile userid userid-feide groups longterm',
-  silent_redirect_uri: `${window.location.protocol}//` +
-    `${window.location.hostname}${window.location.port
-      ? `:${window.location.port}` : ''
-    } / silent_renew.html`,
+  scope: 'email openid profile userid userid-feide groups',
   userStore: new Oidc.WebStorageStateStore({ store: window.localStorage }),
 };
 
