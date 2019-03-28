f948100eaa461c8ae9b3fc833566f1ac69eb55d4
Paalar <paaledward@gmail.com>
2019-03-28 18:35:53 +0100

    update search bar to have a disabled value

--- frontend/src/components/SearchBar/index.tsx
+++ frontend/src/components/SearchBar/index.tsx
@@ -4,16 +4,20 @@ import React, { ChangeEvent, FunctionComponent } from 'react';
 interface IProps {
   filterFunction: (event: ChangeEvent<HTMLInputElement>) => void;
   placeHolder?: string;
+  disabled?: boolean;
 }
 
 const SearchBar: FunctionComponent<IProps> = (props) => {
-  const { placeHolder, filterFunction } = props;
-  const placeHolderText = placeHolder ? placeHolder : 'Search ...';
+  const { filterFunction } = props;
+  const disabled = props.disabled ? props.disabled : false;
+  const placeHolder = props.placeHolder ? props.placeHolder : 'Search ...';
   return (
   <Input
+    className="search-bar"
     label={''}
-    placeholder={placeHolderText}
+    placeholder={placeHolder}
     onChange={filterFunction}
+    disabled={disabled}
   />);
 };
 
