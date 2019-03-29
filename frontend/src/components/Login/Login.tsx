adcd38d4dd09707d6a84d096d56aa8c2d806eae2
adrianht <adrianhthompson@gmail.com>
2019-03-29 12:12:46 +0100

    adds new logo and styles it

--- frontend/src/components/Login/Login.tsx
+++ frontend/src/components/Login/Login.tsx
@@ -1,9 +1,8 @@
 import KnappBase from 'nav-frontend-knapper';
 import * as React from 'react';
-import Logo_Ntnu from '../../assets/Logo-Ntnu.svg';
+import IDILOGO from '../../assets/IDI_Logo.svg';
 import userManager from '../../utils/userManager';
 import './login.css';
-
 export const Login: React.FunctionComponent<{}> = (props) => {
   const doLogin = (event: React.MouseEvent) => {
     event.preventDefault();
@@ -11,26 +10,28 @@ export const Login: React.FunctionComponent<{}> = (props) => {
   };
   const onClickAction = (event: React.MouseEvent) => doLogin(event);
   return (
-    <div className="mainContent">
-      <img src={Logo_Ntnu} id="ntnuLogo" />
-      <h1>
-        Welcome!
-      </h1>
-      <p>
-        This is your portal for applying for designated seats at the
-        Department of Computer Science (IDI).
-      </p>
-      <p>
-        Click the button to login using FEIDE.
-      </p>
-      <KnappBase
-        type="hoved"
-        htmlType="button"
-        onClick={onClickAction}
-        id="loginButton"
-      >
-        Log in
-      </KnappBase>
-    </div>
+    <>
+      <img src={IDILOGO} id="ntnuLogo" />
+      <div className="mainContent">
+        <h1>
+          Welcome!
+        </h1>
+        <p>
+          This is your portal for applying for designated seats at the
+          Department of Computer Science (IDI).
+        </p>
+        <p>
+          Click the button to login using FEIDE.
+        </p>
+        <KnappBase
+          type="hoved"
+          htmlType="button"
+          onClick={onClickAction}
+          id="loginButton"
+        >
+          Log in
+        </KnappBase>
+      </div>
+    </>
   );
 };
