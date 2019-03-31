fa30fc036db8dd5f038d49a5edb1e62ca6f36ce7
s0lvang <august.s.solvang@gmail.com>
2019-03-31 19:51:22 +0200

    filled utils/groups

--- backend/utils/groups.py
+++ backend/utils/groups.py
@@ -1,5 +1,9 @@
 MASTERS = [
+    "fc:fs:fs:emne:ntnu.no:IT3904:1",
     "fc:fs:fs:emne:ntnu.no:IT3903:1",
+    "fc:fs:fs:emne:ntnu.no:IT3906:1",
+    "fc:fs:fs:emne:ntnu.no:IT3901:1",
+    "fc:fs:fs:emne:ntnu.no:TDT4900",
 ]
 
 PROJECT_ASSIGNMENTS = [
@@ -8,12 +12,16 @@ PROJECT_ASSIGNMENTS = [
 ]
 
 MASTER_STUDIES = [
-    "fc:fs:fs:prg:ntnu.no:MIT"
-
+    "fc:fs:fs:prg:ntnu.no:MIT",
+    "fc:fs:fs:prg:ntnu.no:MIDT",
 ]
 
 BACHELOR_STUDIES = [
-    "fc:fs:fs:prg:ntnu.no:BIT"
+    "fc:fs:fs:prg:ntnu.no:BIT",
+    "fc:fs:fs:prg:ntnu.no:BIDATA",
+    "fc:fs:fs:prg:ntnu.no:ITBAITBEDR",
+    "fc:fs:fs:prg:ntnu.no:BDIGSEC",
+    "fc:fs:fs:prg:ntnu.no:BPROG",
 ]
 
 # TODO fill these with all the stuff
