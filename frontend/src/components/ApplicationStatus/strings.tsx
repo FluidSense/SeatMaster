0c98714f45fb5e77aa326bd63b76eb25a2734aa1
August Sollesnes Solvang <august.s.solvang@gmail.com>
2019-03-31 19:00:18 +0200

    fixes spelling error (#311)

--- frontend/src/components/ApplicationStatus/strings.tsx
+++ frontend/src/components/ApplicationStatus/strings.tsx
@@ -1,5 +1,5 @@
 export const _APP_NOT_FOUND = 'We can\'t find an application linked to you';
-export const _APP_SUBMITTED = 'You have submitted and account, it is currently being processed';
+export const _APP_SUBMITTED = 'You have submitted an application, it is currently being processed';
 export const _APP_DENIED = 'Your application has been denied';
 export const _APP_APPROVED = 'Your application has been accepted';
 export const _APP_WAITING = 'You are on the waiting list';
