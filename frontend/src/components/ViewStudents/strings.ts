93b6e22fac8ea4dd221fb706f620164df771e59b
Aslak Hollund <aslakhol@gmail.com>
2019-04-01 21:46:36 +0200

    Frontend/students view enhanced (#337)
    * UserLink > UserPanel
    
    * add email to panel
    
    * tweak strings

--- frontend/src/components/ViewStudents/strings.ts
+++ frontend/src/components/ViewStudents/strings.ts
@@ -1,7 +1,11 @@
 export const _USER_FULL_NAME = 'Full name';
 export const _USER_USERNAME = 'Username';
+export const _USER_APPLICATION_STATUS = 'Application Status';
+export const _USER_EMAIL = 'Email';
 export const _VIEW_STUDENTS_TITLE = 'Students';
 export const _DELETE_STUDENTS = 'Delete students';
-export const _CHECK_CHECKBOX = 'Check';
-export const _CHECK_ALL_CHECKBOXES = 'Check all students';
-export const _DELETING_STUDENTS_WARNING = 'This will delete the following students permanently:';
+// _CHECKBOX_LABEL is an invisible Unicode character ZERO WIDTH NON-JOINER (U+200C)
+// to deal with a quirk in nav checkboxes
+export const _CHECKBOX_LABEL = '‌‌‌‌ ';
+export const _CHECK_ALL_CHECKBOXES = 'Select all students for deletion';
+export const _DELETING_STUDENTS_WARNING = 'This will delete the following students permanently:';
