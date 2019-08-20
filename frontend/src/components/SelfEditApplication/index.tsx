3614833ee6cb8c803ce9c7a4aa6d66a5394d2226
FluidSense <22146437+FluidSense@users.noreply.github.com>
2019-08-20 19:38:16 +0200

    load rooms if not present on edit

--- frontend/src/components/SelfEditApplication/index.tsx
+++ frontend/src/components/SelfEditApplication/index.tsx
@@ -1,10 +1,54 @@
+import React from 'react';
 import { connect } from 'react-redux';
 import { ThunkDispatch } from 'redux-thunk';
 import { IPostApplicationForm } from '../../API/interfaces';
 import { IStore } from '../../store';
 import { resetAppStatus } from '../EditApplication/actions';
 import { updateSelfApplication } from './actions';
+import { fetchAllRooms } from '../ViewRooms/actions';
+import { IRoom } from '../ViewRooms';
 import EditApplication from './Presentational';
+
+interface IDispatchProps {
+  resetStatus: () => void;
+  updateApplication: (app: IPostApplicationForm) => void;
+  getRooms: () => void;
+}
+
+interface IStateProps {
+  rooms?: IRoom[];
+}
+
+interface IOwnState {
+  fetchedRooms: boolean;
+}
+
+type Props = IDispatchProps & IStateProps;
+
+class _Container extends React.Component<Props, IOwnState>{
+  constructor(props: Props){
+    super(props);
+    const hasRooms = this.props.rooms ? this.props.rooms.length > 0 : false;
+    this.state = {
+      fetchedRooms: hasRooms,
+    }
+  }
+  public componentDidMount(){
+    if (!this.state.fetchedRooms) {
+      this.props.getRooms();
+    }
+  }
+
+  public render(){
+    return (
+      <EditApplication
+        rooms={this.props.rooms}
+        application={this.props.application}
+        status={this.props.status}
+      />
+    );
+  }
+}
 
 const mapStateToProps = (state: IStore) => ({
   application: state.applications.registeredApplication,
@@ -14,6 +58,7 @@ const mapStateToProps = (state: IStore) => ({
 
 const mapDispatchToProps = (dispatch: ThunkDispatch<{}, {}, any>) => ({
   resetStatus: () => dispatch(resetAppStatus()),
+  getRooms: () => dispatch(fetchAllRooms()),
   updateApplication: (application: IPostApplicationForm) => {
     return dispatch(updateSelfApplication(application));
   },
@@ -22,6 +67,6 @@ const mapDispatchToProps = (dispatch: ThunkDispatch<{}, {}, any>) => ({
 const SelfEditApplication = connect(
   mapStateToProps,
   mapDispatchToProps,
-)(EditApplication);
+)(_Container);
 
 export default SelfEditApplication;
